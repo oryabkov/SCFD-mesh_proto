bb64a5800135f355f79634352edb65553a04261a
Oleg Ryabkov <oleg.ryabkov.87@gmail.com>
2021-04-03 20:49:57 +0300

    further sparse_arr adaptation

--- include/scfd/mesh/detail/sparse_arr.h
+++ include/scfd/mesh/detail/sparse_arr.h
@@ -29,15 +29,15 @@ namespace detail
 template<class T,class Ord = int>
 struct sparse_arr
 {
-    Ord                  n;
     std::vector<T>       objs_;
+    //TODO for dense numeration map can be avoided (something like GMSH internal cache)
     std::map<Ord,Ord>    glob_ind_2_vec_ind;
 
-    //it's real total mesh size, not number of actually read elements (which could be zero, for example)
-    Ord             size()const 
-    { 
-        return n; 
+    void preallocate(Ord size)
+    {
+        objs_.reserve(size);
     }
+
     Ord             find_elem__(Ord glob_i)const
     {
         std::map<Ord,Ord>::const_iterator       it = glob_ind_2_vec_ind.find(glob_i);
@@ -55,11 +55,11 @@ struct sparse_arr
         return objs_[ find_elem__(glob_i) ];
     }
     //TODO rename has_elem and add_elem into has and and
-    bool            has_elem(Ord glob_i)const
+    bool            has(Ord glob_i)const
     {
         return (glob_ind_2_vec_ind.find(glob_i) != glob_ind_2_vec_ind.end());
     }
-    void            add_elem(Ord glob_i, const T &e)
+    void            add(Ord glob_i, const T &e)
     {
         //ISSUE check if we already have glob_i?? i think do it in DEBUG mode
         glob_ind_2_vec_ind[glob_i] = objs_.size();
