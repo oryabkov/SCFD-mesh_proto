60b053086ad31665b600281ddd622073dad2df70
Oleg Ryabkov <oleg.ryabkov.87@gmail.com>
2021-04-11 00:37:48 +0300

    added empty file for supplementary face_key structure

---
+++ include/scfd/mesh/detail/face_key.h
@@ -0,0 +1,33 @@
+// Copyright © 2016-2021 Ryabkov Oleg Igorevich, Evstigneev Nikolay Mikhaylovitch
+
+// This file is part of SimpleCFD.
+
+// SimpleCFD is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2 only of the License.
+
+// SimpleCFD is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with SimpleCFD.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef __SCFD_MESH_FACE_KEY_H__
+#define __SCFD_MESH_FACE_KEY_H__
+
+namespace scfd
+{
+namespace mesh
+{
+namespace detail
+{
+
+
+
+}  /// namespace detail
+}  /// namespace mesh
+}  /// namespace scfd
+
+#endif
