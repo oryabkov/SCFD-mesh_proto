bdfa4f982c776fb8d413d515a4a7ebd822612e7b
Oleg Ryabkov <oleg.ryabkov.87@gmail.com>
2021-04-13 00:12:20 +0300

    added linear_partitiner (as is from SimpleCFD, just namespace added); started test;

--- test/mesh/test_gmsh_mesh_wrap.cpp
+++ test/mesh/test_gmsh_mesh_wrap.cpp
@@ -1,9 +1,12 @@
 
+#include <scfd/communication/linear_partitioner.h>
 #include <scfd/mesh/gmsh_mesh_wrap.h>
+
+using partitioner_t = scfd::communication::linear_partitioner;
+using gmsh_wrap_t = scfd::mesh::gmsh_mesh_wrap<double,partitioner_t,3>;
 
 int main(int argc, char const *args[])
 {
-
     
     return 0;
 }
