446bede08c4c39e82668ed371ff19e65cdcf5059
Oleg Ryabkov <oleg.ryabkov.87@gmail.com>
2021-04-15 14:05:54 +0300

    added cmake for demos; continue initial fits; added test.msh to demos

--- demos/host_poisson_solver/host_poisson_solver.cpp
+++ demos/host_poisson_solver/host_poisson_solver.cpp
@@ -17,15 +17,18 @@
 #include "host_poisson_solver_config.h"
 #include <string>
 #include <cmath>
-#include <utils/Log.h>
-#include <utils/main_try_catch_macro.h>
+#include <scfd/utils/log_std.h>
+#include <scfd/utils/main_try_catch_macro.h>
 #include <scfd/mesh/gmsh_mesh_wrap.h>
 #include <scfd/mesh/host_mesh.h>
 #include "gmsh_pos_output.h"
 
-typedef HOST_POISSON_SOLVER_SCALAR_TYPE  real;
-typedef t_cpu_mesh_tml<real>             t_cpu_mesh;
-typedef t_vec_tml<real,3>                t_vec;
+using real = HOST_POISSON_SOLVER_SCALAR_TYPE;
+using ordinal = int;
+using gmsh_wrap_t = scfd::mesh::gmsh_mesh_wrap<real,partitioner_t,3,ordinal>;
+using host_mesh_t = scfd::mesh::host_mesh<gmsh_wrap_t>;
+using vec_t = scfd::static_vec::vec<real,3>;
+using log_t = scfd::utils::log_std;
 
 int bnd1, bnd2, iters_num;
 
@@ -39,23 +42,23 @@ t_vec reflect_point(const t_vec &norm, const t_vec &p1, const t_vec &p0)
 }
 
 
-void    poisson_iteration(const t_cpu_mesh &cpu_mesh, const real *vars_old, real *vars_new)
+void    poisson_iteration(const host_mesh_t &host_mesh, const real *vars_old, real *vars_new)
 {
-    for (int i = 0;i < cpu_mesh.cv.size();++i) {
+    for (int i = 0;i < host_mesh.cv.size();++i) {
         real    numerator(0.f), denominator(0.f);
-        for (int j = 0;j < cpu_mesh.cv[i].faces_n;++j) {
-            int nb = cpu_mesh.cv[i].neighbours[j];
+        for (int j = 0;j < host_mesh.cv[i].faces_n;++j) {
+            int nb = host_mesh.cv[i].neighbours[j];
             t_vec   nb_center;
             real    dist, var_nb;
             if (nb != -1) {
-                nb_center = cpu_mesh.cv[nb].center;
+                nb_center = host_mesh.cv[nb].center;
                 var_nb = vars_old[nb];
             } else {
-                nb_center = reflect_point(cpu_mesh.cv[i].norms[j], cpu_mesh.cv[i].face_centers[j], cpu_mesh.cv[i].center);
-                if (cpu_mesh.cv[i].boundaries[j] == bnd1) {
+                nb_center = reflect_point(host_mesh.cv[i].norms[j], host_mesh.cv[i].face_centers[j], host_mesh.cv[i].center);
+                if (host_mesh.cv[i].boundaries[j] == bnd1) {
                     //dirichle 0. value
                     var_nb = -vars_old[i];
-                } else if (cpu_mesh.cv[i].boundaries[j] == bnd2) {
+                } else if (host_mesh.cv[i].boundaries[j] == bnd2) {
                     //dirichle 1. value
                     var_nb = real(2.f)*real(1.f)-vars_old[i];
                 } else {
@@ -63,33 +66,33 @@ void    poisson_iteration(const t_cpu_mesh &cpu_mesh, const real *vars_old, real
                     var_nb = vars_old[i];
                 }
             }
-            dist = scalar_prod(cpu_mesh.cv[i].norms[j], nb_center - cpu_mesh.cv[i].center);
-            numerator += cpu_mesh.cv[i].S[j]*var_nb/dist;
-            denominator += cpu_mesh.cv[i].S[j]/dist;
+            dist = scalar_prod(host_mesh.cv[i].norms[j], nb_center - host_mesh.cv[i].center);
+            numerator += host_mesh.cv[i].S[j]*var_nb/dist;
+            denominator += host_mesh.cv[i].S[j]/dist;
         }
                 vars_new[i] = numerator/denominator;
     }
 }
 
-void    fill_zero(const t_cpu_mesh &cpu_mesh, real *A)
+void    fill_zero(const host_mesh_t &host_mesh, real *A)
 {
-    for (int i = 0;i < cpu_mesh.cv.size();++i) {
+    for (int i = 0;i < host_mesh.cv.size();++i) {
         A[i] = 0.f;
     }
 }
 
 //B := A
-void    assign(const t_cpu_mesh &cpu_mesh, real *B, const real *A)
+void    assign(const host_mesh_t &host_mesh, real *B, const real *A)
 {
-    for (int i = 0;i < cpu_mesh.cv.size();++i) {
+    for (int i = 0;i < host_mesh.cv.size();++i) {
         B[i] = A[i];
     }
 }
 
 int main(int argc, char **args)
 {
-    LogStd          log;
-    t_cpu_mesh      cpu_mesh;
+    log_t           log;
+    host_mesh_t     host_mesh;
     real            *vars0, *vars1;
 
     USE_MAIN_TRY_CATCH(log)
@@ -98,7 +101,7 @@ int main(int argc, char **args)
     
     //process args
     if (argc < 4) {
-        printf("usage: cpu_poisson_solver bnd1_id bnd2_id iters_num\n");
+        printf("usage: host_poisson_solver bnd1_id bnd2_id iters_num\n");
         printf("example: ./cpu_poisson_solver 6 166 1000\n");
         return 1;
     }
@@ -107,28 +110,28 @@ int main(int argc, char **args)
     iters_num = atoi(args[3]);
 
     MAIN_TRY("reading mesh from mesh.dat")
-        if (!cpu_mesh.read("mesh.dat")) throw std::runtime_error("failed to read mesh from mesh.dat");
+        if (!host_mesh.read("mesh.dat")) throw std::runtime_error("failed to read mesh from mesh.dat");
     MAIN_CATCH(2)
 
     MAIN_TRY("allocating variables array")
-        vars0 = new real[cpu_mesh.cv.size()];
-        vars1 = new real[cpu_mesh.cv.size()];
+        vars0 = new real[host_mesh.cv.size()];
+        vars1 = new real[host_mesh.cv.size()];
         MAIN_CATCH(3)
 
         MAIN_TRY("iterate poisson equation")
-            fill_zero(cpu_mesh, vars0);
-            fill_zero(cpu_mesh, vars1);
+            fill_zero(host_mesh, vars0);
+            fill_zero(host_mesh, vars1);
             for (int i = 0;i < iters_num;++i) {
             log.info_f("iteration %d", i);
             //put result in vars1
-            poisson_iteration(cpu_mesh, vars0, vars1);
+            poisson_iteration(host_mesh, vars0, vars1);
             //vars0 := vars1
-                        assign(cpu_mesh, vars0, vars1);
+                        assign(host_mesh, vars0, vars1);
         }
         MAIN_CATCH(4)
 
         MAIN_TRY("writing pos output to result.pos")
-            write_out_pos_scalar_file("result.pos", "poisson_phi", cpu_mesh, vars0);
+            write_out_pos_scalar_file("result.pos", "poisson_phi", host_mesh, vars0);
         MAIN_CATCH(5)
 
         MAIN_TRY("deallocating variables array")
