f900a687db6bd245a19ef3cab3c3d8105d2b8cbf
Oleg Ryabkov <oleg.ryabkov.87@gmail.com>
2021-06-22 15:46:14 +0300

    added second periodic test - not working for now - as is

--- test/mesh/test_host_mesh_gmsh_wrap.cpp
+++ test/mesh/test_host_mesh_gmsh_wrap.cpp
@@ -203,3 +203,128 @@ TEST(TestHostMeshGMSHWrap, BasicReadPeriodic1)
     }    
 }
 
+TEST(TestHostMeshGMSHWrap, BasicReadPeriodic2)
+{
+    try 
+    {
+        auto        part = std::make_shared<partitioner_t>();
+        auto        host_mesh = std::make_shared<host_mesh_t>();
+        host_mesh->set_mesh_filename("test_box3d_period_small_mesh.msh");
+        /// No periodic surfaces is set (so, mesh is not periodic)
+        host_mesh->read(std::set<ordinal>({5,27}));
+        *part = partitioner_t(host_mesh->get_total_elems_num(), 1, 0);
+        host_mesh->set_partitioner(part);
+        host_mesh->enlarge_stencil(1);
+
+        ASSERT_EQ(host_mesh->get_total_nodes_num(), 14);
+        /*for (ordinal i = 1;i <= 14;++i)
+        {
+            ASSERT_EQ(host_mesh->get_node_virt_master_id(i), i);
+        }*/
+        ASSERT_EQ(host_mesh->get_node_virt_master_id(1 ), 1 );
+        ASSERT_EQ(host_mesh->get_node_virt_master_id(2 ), 2 );
+        ASSERT_EQ(host_mesh->get_node_virt_master_id(3 ), 3 );
+        ASSERT_EQ(host_mesh->get_node_virt_master_id(4 ), 4 );
+        ASSERT_EQ(host_mesh->get_node_virt_master_id(5 ), 1 );
+        ASSERT_EQ(host_mesh->get_node_virt_master_id(6 ), 2 );
+        ASSERT_EQ(host_mesh->get_node_virt_master_id(7 ), 4 );
+        ASSERT_EQ(host_mesh->get_node_virt_master_id(8 ), 3 );
+        ASSERT_EQ(host_mesh->get_node_virt_master_id(9 ), 9 );
+        ASSERT_EQ(host_mesh->get_node_virt_master_id(10), 10);
+        ASSERT_EQ(host_mesh->get_node_virt_master_id(11), 11);
+        ASSERT_EQ(host_mesh->get_node_virt_master_id(12), 12);
+        ASSERT_EQ(host_mesh->get_node_virt_master_id(13), 13);
+        ASSERT_EQ(host_mesh->get_node_virt_master_id(14), 9 );
+
+        ordinal prim_nodes_num; 
+        ordinal nodes[4];
+        host_mesh->get_elem_prim_nodes(65-45, nodes, &prim_nodes_num);
+        ASSERT_EQ(prim_nodes_num, 4);
+        ASSERT_EQ(nodes[0], 13);
+        ASSERT_EQ(nodes[1], 8);
+        ASSERT_EQ(nodes[2], 14);
+        ASSERT_EQ(nodes[3], 12);
+
+        ordinal prim_virt_nodes_num; 
+        ordinal virt_nodes[4];
+        host_mesh->get_elem_prim_virt_nodes(65-45, virt_nodes, &prim_virt_nodes_num);
+        ASSERT_EQ(prim_virt_nodes_num, 4);
+        ASSERT_EQ(virt_nodes[0], 13);
+        ASSERT_EQ(virt_nodes[1], 3);
+        ASSERT_EQ(virt_nodes[2], 9);
+        ASSERT_EQ(virt_nodes[3], 12);
+
+        ordinal elems_num0; 
+        ordinal elems0[4];
+        ASSERT_EQ(host_mesh->get_node_incident_elems_num(8),4);
+        host_mesh->get_node_incident_elems(8,elems0,&elems_num0);
+        ASSERT_EQ(elems_num0, 4);
+        ///TODO order is in fact not garanteed here
+        ASSERT_EQ(elems0[0], 50-45);
+        ASSERT_EQ(elems0[1], 51-45);
+        ASSERT_EQ(elems0[2], 56-45);
+        ASSERT_EQ(elems0[3], 65-45);
+
+        ordinal elems_num; 
+        ordinal elems[8];
+        /// NOTE here we use 3 instead of 8 because there is not virtual node '8'
+        ASSERT_EQ(host_mesh->get_virt_node_incident_elems_num(3),8);
+        host_mesh->get_virt_node_incident_elems(3,elems,&elems_num);
+        ASSERT_EQ(elems_num, 8);
+        ///TODO order is in fact not garanteed here
+        ASSERT_EQ(elems[0], 49-45);
+        ASSERT_EQ(elems[1], 50-45);
+        ASSERT_EQ(elems[2], 51-45);
+        ASSERT_EQ(elems[3], 51-45);
+        ASSERT_EQ(elems[4], 56-45);
+        ASSERT_EQ(elems[5], 58-45);
+        ASSERT_EQ(elems[6], 62-45);
+        ASSERT_EQ(elems[7], 65-45);
+
+        ASSERT_EQ(host_mesh->get_elem_type(54-45),MSH_TET_4);
+        ASSERT_EQ(host_mesh->get_elem_type(64-45),MSH_TET_4);
+        ASSERT_EQ(host_mesh->get_elem_type(67-45),MSH_TET_4);
+        ASSERT_EQ(host_mesh->get_elem_type(50-45),MSH_TET_4);
+        ASSERT_EQ(host_mesh->get_elem_type(60-45),MSH_TET_4);
+        ASSERT_EQ(host_mesh->get_elem_group_id(54-45),1);
+        ASSERT_EQ(host_mesh->get_elem_group_id(64-45),1);
+        ASSERT_EQ(host_mesh->get_elem_group_id(67-45),1);
+        ASSERT_EQ(host_mesh->get_elem_group_id(50-45),1);
+        ASSERT_EQ(host_mesh->get_elem_group_id(60-45),1);
+        //test neigbours0 54-45 
+        ASSERT_EQ(host_mesh->get_elem_faces_num(54-45),4);
+        ordinal                 elem_54_neibs0[host_mesh->get_elem_faces_num(54-45)];
+        host_mesh->get_elem_neighbours0(54-45, elem_54_neibs0);
+        std::sort(elem_54_neibs0,elem_54_neibs0+host_mesh->get_elem_faces_num(54-45));
+        ordinal                 elem_54_neibs0_ref[] = {64-45,67-45,host_mesh_t::special_id,host_mesh_t::special_id};
+        std::sort(elem_54_neibs0_ref,elem_54_neibs0_ref+4);
+        for (ordinal i = 0;i < 4;++i)
+        {
+            ASSERT_EQ(elem_54_neibs0[i],elem_54_neibs0_ref[i]);
+        }
+
+        //test neigbours0 54-45 
+        ASSERT_EQ(host_mesh->get_elem_faces_num(54-45),4);
+        ordinal                 elem_54_virt_neibs0[host_mesh->get_elem_faces_num(54-45)];
+        host_mesh->get_elem_virt_neighbours0(54-45, elem_54_virt_neibs0);
+        std::sort(elem_54_virt_neibs0,elem_54_virt_neibs0+host_mesh->get_elem_faces_num(54-45));
+        ordinal                 elem_54_virt_neibs0_ref[] = {60-45,64-45,67-45,host_mesh_t::special_id};
+        std::sort(elem_54_virt_neibs0_ref,elem_54_virt_neibs0_ref+4);
+        for (ordinal i = 0;i < 4;++i)
+        {
+            std::cout << elem_54_virt_neibs0[i] << " " << elem_54_virt_neibs0_ref[i] << std::endl;
+        }
+        for (ordinal i = 0;i < 4;++i)
+        {
+            ASSERT_EQ(elem_54_virt_neibs0[i],elem_54_virt_neibs0_ref[i]);
+        }
+    } 
+    catch(const std::exception &e)
+    {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        std::cerr << "exit" << std::endl;
+        FAIL();
+    }    
+}
+
+
